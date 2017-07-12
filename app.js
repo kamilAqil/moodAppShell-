a68946ab71515d7b44f53532aa9e277f44a4cb72
mehdicode <kalan.mahdi@yahoo.com>
2017-07-12 03:09:55 -0700

    logout button

--- app.js
+++ app.js
@@ -50,6 +50,10 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res, next){
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
