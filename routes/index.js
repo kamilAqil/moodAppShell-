67163f0f270a2271c3c7803e69130b3df8e0dd1a
Kamil <kaqil91@gmail.com>
2017-07-12 13:38:34 -0700

    starting to set up process.env

--- routes/index.js
+++ routes/index.js
@@ -44,8 +44,9 @@ router.post('/register', function(req, res, next) {
     req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
     req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
     req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-    req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
-    req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+	req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
+	// going to turn off the password validation must include lowercase just for dev desting
+    // req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
     req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
     req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
 
