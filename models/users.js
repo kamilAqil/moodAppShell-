9bb6ebf398ecdd5af41f2bf86771399c18ae9dee
Kamil <kaqil91@gmail.com>
2017-07-12 20:04:28 -0700

    users model works

---
+++ models/users.js
@@ -0,0 +1,44 @@
+module.exports = function(sequelize,DataTypes){
+    var users = sequelize.define("users", {
+ 
+
+
+ 
+        username: {
+            type: DataTypes.STRING(15),
+            allowNull: false
+        },
+
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+ 
+        password: {
+            type: DataTypes.STRING(60),
+            allowNull: false
+        }
+ 
+ 
+    },
+
+        {
+            
+            classMethods: {
+                associate: function(models) {
+
+                    Customer.hasMany(models.Post, {
+                        onDelete: "cascade"
+                    });
+                }
+            }
+        }
+);
+return users;
+
+// Syncs with DB
+//users.sync();
+}
